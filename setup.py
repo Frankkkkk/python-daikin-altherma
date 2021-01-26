423fadd017cf17d6914ad9b5674929244564a16e
Frank Villaro-Dixon <frank@vi-di.fr>
2021-01-26 18:33:46 +0100

    Move things around

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-daikin-altherma",
+    version = "0.0.1",
+    author = "Frank Villaro-Dixon",
+    author_email = "frank@villaro-dixon.eu",
+    description = ("Connect to daikin altherma heat pumps"),
+    license = "MIT",
+    keywords = "daikin altherma heat pump",
+    url = "http://github.com/Frankkkkk/python-daikin-altherma",
+    packages=['daikin_altherma'],
+    long_description='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
