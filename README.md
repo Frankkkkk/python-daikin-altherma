7d171b0db9e14bfd7b9276b6b6a9adf4dc486a7b
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2023-10-20 18:30:22 +0200

    Add much more things
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ talks to daikin altherma via LAN adapter BRP069A61 or BRP069A62
 
 # How to use
 ## How to install ?
-Simply type `pip3 install python-daikin-altherma`
+Simply get it from [pypi](https://pypi.org/project/python-daikin-altherma/) by:
+`pip3 install python-daikin-altherma`
 
 ## How to run
 ```python3
@@ -11,7 +12,26 @@ Simply type `pip3 install python-daikin-altherma`
 >>> d = DaikinAltherma('192.168.10.126')
 >>> print(f'My outdoor temperature is {d.outdoor_temperature}°C')
 My outdoor temperature is 2.0°C
+>>> d.print_all_status()
+Daikin adapter: 192.168.11.100 BRP069A61
+Daikin unit: EAVH16S23DA6V 0
+Daikin time: 2023-10-20 18:56:08
+Hot water tank:
+    Current: 48.0°C (target 49.0°C)
+    Heating enabled: True (Powerful: False)
+Heating:
+    Outdoor temp:14.0°C
+    Indoor temp: 21.5°C
+    Heating target: 24.0°C (is heating enabled: True)
+    Leaving water: 29.0°C
+    Heating mode: heating
+    Schedule: {'Mo': {'0000': 18.0, '0600': 20.0, '1200': 22.0, '1500': 20.0, '1800': 18.0}, 'Tu': {'0000': 18.0, '0600': 20.0, '1800': 18.0}, 'We': {'0000': 18.0, '0600': 20.0, '1800': 18.0}, 'Th': {'0000': 18.0, '0600': 20.0, '1200': 22.0, '1500': 20.0, '1800': 18.0}, 'Fr': {'0000': 18.0, '0600': 20.0, '1200': 22.0, '1500': 20.0, '1800': 18.0}, 'Sa': {'0000': 18.0, '0600': 20.0, '1200': 22.0, '1500': 20.0, '1800': 18.0}, 'Su': {'0000': 18.0, '0600': 20.0, '1200': 22.0, '1500': 20.0, '1800': 18.0}}
+
 ```
+
+## Schedules
+You can set schedules using `set_heating_schedule(schedule)`. Your best bet is to
+look at the [example file](example.py).
 
 # Documentation
 ```
@@ -23,22 +43,32 @@ My outdoor temperature is 2.0°C
      |  __init__(self, adapter_ip: str)
      |      Initialize self.  See help(type(self)) for accurate signature.
      |  
-     |  set_heating(self, heating_active: bool)
+     |  print_all_status(self)
+     |  
+     |  set_heating_enabled(self, heating_active: bool)
      |      Whether to turn the heating on(True) or off(False).
-     |      You can confirm that it works by calling self.power_state
+     |      You can confirm that it works by calling self.is_heating_enabled
+     |  
+     |  set_heating_schedule(self, schedule: dict[str, dict[str, float]])
+     |      Sets the heating schedule for the heating.
      |  
      |  set_setpoint_temperature(self, setpoint_temperature_c: float)
      |      Sets the heating setpoint (target) temperature, in °C
      |  
-     |  set_tank_heating(self, powerful_active: bool)
+     |  set_tank_heating_enabled(self, powerful_active: bool)
      |      Whether to turn the water tank heating on(True) or off(False).
-     |      You can confirm that it works by calling self.tank_powerful_state
+     |      You can confirm that it works by calling self.is_tank_heating_enabled
      |  
      |  ----------------------------------------------------------------------
      |  Readonly properties defined here:
      |  
      |  adapter_model
-     |      Returns the model of the LAN adapter
+     |      Returns the model of the LAN adapter.
+     |      Ex: BRP069A61
+     |  
+     |  heating_mode
+     |      This function name makes no sense, because it
+     |      returns whether the heat pump is heating or cooling.
      |  
      |  indoor_setpoint_temperature
      |      Returns the indoor setpoint (target) temperature, in °C
@@ -46,27 +76,80 @@ My outdoor temperature is 2.0°C
      |  indoor_temperature
      |      Returns the indoor temperature, in °C
      |  
+     |  indoor_unit_software_version
+     |      Returns the unit software version
+     |  
+     |  indoor_unit_version
+     |      Returns the unit version
+     |  
+     |  is_heating_enabled
+     |      Returns if the unit heating is enabled
+     |  
+     |  is_tank_heating_enabled
+     |      Returns if the tank heating is currently enabled
+     |  
+     |  is_tank_powerful
+     |      Returns if the tank is in powerful state
+     |  
      |  leaving_water_temperature
      |      Returns the heating leaving water temperature, in °C
      |  
      |  outdoor_temperature
      |      Returns the outdoor temperature, in °C
      |  
+     |  outdoor_unit_software_version
+     |      Returns the unit software version
+     |  
+     |  pin_code
+     |      Returns the pin code of the LAN adapter
+     |  
      |  power_consumption
      |      Returns the energy consumption in kWh per [D]ay, [W]eek, [M]onth
      |  
-     |  power_state
-     |      Returns the power state
+     |  remote_setting_version
+     |      Returns the remote console setting version
      |  
-     |  tank_power_state
-     |      Returns the tank power state
+     |  remote_software_version
+     |      Returns the remote console setting software version
      |  
-     |  tank_powerful_state
-     |      Returns the tank powerful state
+     |  schedule_list_heating
+     |      Returns the Schedule list heating
+     |  
+     |  schedule_next
+     |      What will happen next the temperature
+     |  
+     |  tank_setpoint_temperature
+     |      Returns the hot water tank setpoint (target) temperature, in °C
      |  
      |  tank_temperature
      |      Returns the hot water tank temperature, in °C
      |  
+     |  unit_datetime
+     |      Returns the current date of the unit. Takes time to refresh
+     |  
+     |  unit_model
+     |      Returns the model of the heating unit.
+     |      Ex: EAVH16S23DA6V
+     |  
+     |  unit_type
+     |      Returns the type of unit
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data descriptors defined here:
+     |  
+     |  __dict__
+     |      dictionary for instance variables (if defined)
+     |  
+     |  __weakref__
+     |      list of weak references to the object (if defined)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes defined here:
+     |  
+     |  DAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+     |  
+     |  UserAgent = 'python-daikin-altherma'
+
 ```
 
 # Acknowledgments
--- daikin_altherma/__init__.py
+++ daikin_altherma/__init__.py
@@ -1,12 +1,21 @@
 import json
+import logging
+import time
 import uuid
+import datetime
 
 from websocket import create_connection
 import dpath.util
 
+Day = Hour = str
+Temperature = float
+Schedule = dict[Day, dict[Hour, Temperature]]
+
 
 class DaikinAltherma:
     UserAgent = "python-daikin-altherma"
+    DAYS = ['Mo','Tu','We','Th', 'Fr', 'Sa', 'Su']
+
 
     def __init__(self, adapter_ip: str):
         self.adapter_ip = adapter_ip
@@ -39,17 +48,79 @@ class DaikinAltherma:
         assert result["m2m:rsp"]["rqi"] == reqid
         assert result["m2m:rsp"]["to"] == DaikinAltherma.UserAgent
 
-        return dpath.util.get(result, output_path)
+        print(output_path)
+        print(f'RES: >>> {result}')
+
+        try:
+            return dpath.util.get(result, output_path)
+        except:
+            logging.error('Could not get data. Maybe the unit is rebooting ?')
+            raise
+
 
     def _requestValueHP(self, item: str, output_path: str, payload=None):
         return self._requestValue(f"MNAE/{item}", output_path, payload)
 
     @property
+    def _unit_api(self):
+        return json.loads(self._requestValueHP("1/UnitProfile/la", "/m2m:rsp/pc/m2m:cin/con"))
+
+    @property
     def adapter_model(self) -> str:
-        """ Returns the model of the LAN adapter """
+        """ Returns the model of the LAN adapter.
+         Ex: BRP069A61 """
         # either BRP069A61 or BRP069A62
         return self._requestValue("MNCSE-node/deviceInfo", "/m2m:rsp/pc/m2m:dvi/mod")
 
+    @property
+    def unit_datetime(self) -> datetime.datetime:
+        """ Returns the current date of the unit. Takes time to refresh """
+        d = self._requestValueHP("0/DateTime/la", "/m2m:rsp/pc/m2m:cin/con")
+        return datetime.datetime.strptime(d, '%Y%m%dT%H%M%SZ')
+
+
+    @property
+    def unit_model(self) -> str:
+        """ Returns the model of the heating unit.
+         Ex: EAVH16S23DA6V """
+        return self._requestValueHP("1/UnitInfo/ModelNumber/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def unit_type(self) -> str:
+        """ Returns the type of unit """
+        return self._requestValueHP("1/UnitInfo/UnitType/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def indoor_unit_version(self) -> str:
+        """ Returns the unit version """
+        return self._requestValueHP("1/UnitInfo/Version/IndoorSettings/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def indoor_unit_software_version(self) -> str:
+        """ Returns the unit software version """
+        return self._requestValueHP("1/UnitInfo/Version/IndoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def outdoor_unit_software_version(self) -> str:
+        """ Returns the unit software version """
+        return self._requestValueHP("1/UnitInfo/Version/OutdoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def remote_setting_version(self) -> str:
+        """ Returns the remote console setting version """
+        return self._requestValueHP("1/UnitInfo/Version/RemoconSettings/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def remote_software_version(self) -> str:
+        """ Returns the remote console setting software version """
+        return self._requestValueHP("1/UnitInfo/Version/RemoconSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    @property
+    def pin_code(self) -> str:
+        """ Returns the pin code of the LAN adapter """
+        return self._requestValueHP("1/ChildLock/PinCode/la", "/m2m:rsp/pc/m2m:cin/con")
+
+    # HOT WATER TANK STUFF
     @property
     def tank_temperature(self) -> float:
         """ Returns the hot water tank temperature, in °C """
@@ -57,6 +128,41 @@ class DaikinAltherma:
             "2/Sensor/TankTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
+    @property
+    def tank_setpoint_temperature(self) -> float:
+        """ Returns the hot water tank setpoint (target) temperature, in °C """
+        return self._requestValueHP(
+            "2/Operation/TargetTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
+
+    @property
+    def is_tank_heating_enabled(self) -> bool:
+        """ Returns if the tank heating is currently enabled"""
+        return self._requestValueHP("2/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con") == "on"
+
+    @property
+    def is_tank_powerful(self) -> bool:
+        """ Returns if the tank is in powerful state """
+        return self._requestValueHP("2/Operation/Powerful/la", "m2m:rsp/pc/m2m:cin/con") == 1
+
+    def set_tank_heating_enabled(self, powerful_active: bool):
+        """ Whether to turn the water tank heating on(True) or off(False).
+        You can confirm that it works by calling self.is_tank_heating_enabled
+        """
+        mode_dict = {
+            True: 1,
+            False: 0,
+        }
+
+        payload = {
+            'con': mode_dict[powerful_active],
+            'cnf': 'text/plain:0',
+        }
+
+        self._requestValueHP("2/Operation/Powerful", "/", payload)
+
+
+    # HEATING STUFF
     @property
     def indoor_temperature(self) -> float:
         """ Returns the indoor temperature, in °C """
@@ -86,20 +192,16 @@ class DaikinAltherma:
         )
 
     @property
-    def power_state(self) -> bool:
-        """ Returns the power state """
+    def is_heating_enabled(self) -> bool:
+        """ Returns if the unit heating is enabled"""
         return self._requestValueHP("1/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con") == "on"
 
     @property
-    def tank_power_state(self) -> bool:
-        """ Returns the tank power state """
-        return self._requestValueHP("2/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con") == "on"
-
-
-    @property
-    def tank_powerful_state(self) -> bool:
-        """ Returns the tank powerful state """
-        return self._requestValueHP("2/Operation/Powerful/la", "m2m:rsp/pc/m2m:cin/con") == 1
+    def heating_mode(self) -> str:
+        """ This function name makes no sense, because it
+        returns whether the heat pump is heating or cooling.
+        """
+        return self._requestValueHP("1/Operation/OperationMode/la", "m2m:rsp/pc/m2m:cin/con")
 
     @property
     def power_consumption(self) -> dict:
@@ -115,9 +217,9 @@ class DaikinAltherma:
 
         self._requestValueHP("1/Operation/TargetTemperature", "/", payload)
 
-    def set_heating(self, heating_active: bool):
+    def set_heating_enabled(self, heating_active: bool):
         """ Whether to turn the heating on(True) or off(False).
-        You can confirm that it works by calling self.power_state
+        You can confirm that it works by calling self.is_heating_enabled
         """
         mode_dict = {
             True: 'on',
@@ -132,32 +234,104 @@ class DaikinAltherma:
         self._requestValueHP("1/Operation/Power", "/", payload)
 
 
-    def set_tank_heating(self, powerful_active: bool):
-        """ Whether to turn the water tank heating on(True) or off(False).
-        You can confirm that it works by calling self.tank_powerful_state
-        """
-        mode_dict = {
-            True: 1,
-            False: 0,
-        }
+    @property
+    def schedule_list_heating(self) -> list[Schedule]:
+        """ Returns the Schedule list heating """
+        d = self._requestValueHP("1/Schedule/List/Heating/la", "/m2m:rsp/pc/m2m:cin/con")
+        j = json.loads(d)
+
+        out_schedules = []
+        for schedule in j['data']:
+            out_schedules.append(self._unmarshall_schedule(schedule))
+        return out_schedules
+
+    def set_heating_schedule(self, schedule: Schedule):
+        ''' Sets the heating schedule for the heating. '''
+        schedule_str = self._marshall_schedule(schedule)
+        dq = {'data': [schedule_str]}
 
         payload = {
-            'con': mode_dict[powerful_active],
+            'con': json.dumps(dq),
             'cnf': 'text/plain:0',
         }
+        self._requestValueHP("1/Schedule/List/Heating", "/", payload)
 
-        self._requestValueHP("2/Operation/Powerful", "/", payload)
+#    @property
+#    def is_heating_schedule_enabled(self):
+#        return self._requestValueHP("1/Schedule/Active/la",  "/m2m:rsp/pc/m2m:cin/con")
 
-if __name__ == "__main__":
-    ad = DaikinAltherma("192.168.10.126")
-    print(ad.adapter_model)
-    print(ad.tank_temperature)
-    print(ad.outdoor_temperature)
-    print(ad.indoor_temperature)
-    print(ad.leaving_water_temperature)
-    print(ad.power_state)
-    print(ad.power_consumption)
-    ad.set_heating(True)
-    print(ad.power_state)
-    print(ad.tank_power_state)
-    print(ad.tank_powerful_state)
+
+    @property
+    def schedule_next(self) -> Schedule:
+        """ What will happen next the temperature """
+        d = self._requestValueHP("1/Schedule/Next/la", "/m2m:rsp/pc/m2m:cin/con")
+        j = json.loads(d)
+        return self._unmarshall_schedule(j)
+
+
+    def print_all_status(self):
+        print(f'''
+Daikin adapter: {self.adapter_ip} {self.adapter_model}
+Daikin unit: {self.unit_model} {self.unit_type}
+Daikin time: {self.unit_datetime}
+Hot water tank:
+    Current: {self.tank_temperature}°C (target {self.tank_setpoint_temperature}°C)
+    Heating enabled: {self.is_tank_heating_enabled} (Powerful: {self.is_tank_powerful})
+Heating:
+    Outdoor temp:{self.outdoor_temperature}°C
+    Indoor temp: {self.indoor_temperature}°C
+    Heating target: {self.indoor_setpoint_temperature}°C (is heating enabled: {self.is_heating_enabled})
+    Leaving water: {self.leaving_water_temperature}°C
+    Heating mode: {self.heating_mode}
+    Schedule: {self.schedule_list_heating[0]}
+    ''')
+
+
+    @staticmethod
+    def _unmarshall_schedule(schedule_str: str) -> Schedule:
+        ''' Converts a schedule string to a schedule dict.
+        The dict keys are the days (DaikinAltherma.DAYS), and the
+        values are a dict of hour (HHMM) -> Setpoint T°
+        Ex: {'Mo': {'0000': 23.4}}'''
+
+        hrs = schedule_str.split('|')[2].split(';')
+        i = 0
+        schedule = {}
+
+        for day in DaikinAltherma.DAYS:
+            schedule_wk = {}
+            temps = hrs[i:i+6]
+            for c in temps:
+                ctime, ctemp = c.split(',')
+                if ctime == '':
+                    continue
+                ctemp = float(ctemp)/10
+                schedule_wk[ctime] = ctemp
+
+            i += 6
+            schedule[day] = schedule_wk
+        return schedule
+
+
+    @staticmethod
+    def _marshall_schedule(schedule: Schedule) -> str:
+        '''' Converts a schedule dict to a Daikin schedule string'''
+        week_schedule = []
+        for day in DaikinAltherma.DAYS:
+            sday = schedule.get(day, {})
+            schedule_day = []
+
+            assert(len(sday) <= 6)
+            for hour in sorted(sday.keys()):
+                schedule_day.append(f'{hour},{int(sday[hour]*10)}')
+            padding_schedule = 6-len(schedule_day)
+            schedule_day += [','] * padding_schedule
+            week_schedule += schedule_day
+        schedule_str = '$NULL|1|' + ';'.join(week_schedule)
+        return schedule_str
+
+if __name__ == '__main__':
+    ad = DaikinAltherma("192.168.11.100")
+    ad.print_all_status()
+    import pprint
+    pprint.pprint(ad._unit_api)
