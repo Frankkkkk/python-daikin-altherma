a302f143c1fde7a221f2af2a7c59e30d9ea7c239
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2023-10-29 13:15:14 +0100

    Py: blackize everything
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- daikin_altherma/__init__.py
+++ daikin_altherma/__init__.py
@@ -13,16 +13,16 @@ Day = Hour = str
 Temperature = float
 Schedule = dict[Day, dict[Hour, Temperature]]
 
+
 class TankScheduleEnum(enum.Enum):
-    OFF = 'off'
-    COMFORT = 'comfort'
-    ECO = 'eco'
+    OFF = "off"
+    COMFORT = "comfort"
+    ECO = "eco"
 
 
 class DaikinAltherma:
     UserAgent = "python-daikin-altherma"
-    DAYS = ['Mo','Tu','We','Th', 'Fr', 'Sa', 'Su']
-
+    DAYS = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
 
     def __init__(self, adapter_ip: str):
         self.adapter_ip = adapter_ip
@@ -40,14 +40,13 @@ class DaikinAltherma:
         }
         if payload:
             set_value_params = {
-                'ty': 4,
-                'op': 1,
-                'pc': {
-                    'm2m:cin': payload,
-                }
+                "ty": 4,
+                "op": 1,
+                "pc": {
+                    "m2m:cin": payload,
+                },
             }
-            js_request['m2m:rqp'].update(set_value_params)
-
+            js_request["m2m:rqp"].update(set_value_params)
 
         self.ws.send(json.dumps(js_request))
         result = json.loads(self.ws.recv())
@@ -58,99 +57,117 @@ class DaikinAltherma:
         try:
             return dpath.util.get(result, output_path)
         except:
-            logging.error('Could not get data. Maybe the unit is starting up?')
+            logging.error("Could not get data. Maybe the unit is starting up?")
             raise
-
 
     def _requestValueHP(self, item: str, output_path: str, payload=None):
         return self._requestValue(f"MNAE/{item}", output_path, payload)
 
     @property
     def _unit_api(self):
-        return json.loads(self._requestValueHP("1/UnitProfile/la", "/m2m:rsp/pc/m2m:cin/con"))
+        return json.loads(
+            self._requestValueHP("1/UnitProfile/la", "/m2m:rsp/pc/m2m:cin/con")
+        )
 
     @property
     def adapter_model(self) -> str:
-        """ Returns the model of the LAN adapter.
-         Ex: BRP069A61 """
+        """Returns the model of the LAN adapter.
+        Ex: BRP069A61"""
         # either BRP069A61 or BRP069A62
         return self._requestValue("MNCSE-node/deviceInfo", "/m2m:rsp/pc/m2m:dvi/mod")
 
     @property
     def unit_datetime(self) -> datetime.datetime:
-        """ Returns the current date of the unit. Takes time to refresh """
+        """Returns the current date of the unit. Takes time to refresh"""
         d = self._requestValueHP("0/DateTime/la", "/m2m:rsp/pc/m2m:cin/con")
-        return datetime.datetime.strptime(d, '%Y%m%dT%H%M%SZ')
-
+        return datetime.datetime.strptime(d, "%Y%m%dT%H%M%SZ")
 
     @property
     def unit_model(self) -> str:
-        """ Returns the model of the heating unit.
-         Ex: EAVH16S23DA6V """
-        return self._requestValueHP("1/UnitInfo/ModelNumber/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the model of the heating unit.
+        Ex: EAVH16S23DA6V"""
+        return self._requestValueHP(
+            "1/UnitInfo/ModelNumber/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def unit_type(self) -> str:
-        """ Returns the type of unit """
+        """Returns the type of unit"""
         return self._requestValueHP("1/UnitInfo/UnitType/la", "/m2m:rsp/pc/m2m:cin/con")
 
     @property
     def indoor_unit_version(self) -> str:
-        """ Returns the unit version """
-        return self._requestValueHP("1/UnitInfo/Version/IndoorSettings/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the unit version"""
+        return self._requestValueHP(
+            "1/UnitInfo/Version/IndoorSettings/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def indoor_unit_software_version(self) -> str:
-        """ Returns the unit software version """
-        return self._requestValueHP("1/UnitInfo/Version/IndoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the unit software version"""
+        return self._requestValueHP(
+            "1/UnitInfo/Version/IndoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def outdoor_unit_software_version(self) -> str:
-        """ Returns the unit software version """
-        return self._requestValueHP("1/UnitInfo/Version/OutdoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the unit software version"""
+        return self._requestValueHP(
+            "1/UnitInfo/Version/OutdoorSoftware/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def remote_setting_version(self) -> str:
-        """ Returns the remote console setting version """
-        return self._requestValueHP("1/UnitInfo/Version/RemoconSettings/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the remote console setting version"""
+        return self._requestValueHP(
+            "1/UnitInfo/Version/RemoconSettings/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def remote_software_version(self) -> str:
-        """ Returns the remote console setting software version """
-        return self._requestValueHP("1/UnitInfo/Version/RemoconSoftware/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the remote console setting software version"""
+        return self._requestValueHP(
+            "1/UnitInfo/Version/RemoconSoftware/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def pin_code(self) -> str:
-        """ Returns the pin code of the LAN adapter """
+        """Returns the pin code of the LAN adapter"""
         return self._requestValueHP("1/ChildLock/PinCode/la", "/m2m:rsp/pc/m2m:cin/con")
 
     # HOT WATER TANK STUFF
     @property
     def tank_temperature(self) -> float:
-        """ Returns the hot water tank temperature, in °C """
+        """Returns the hot water tank temperature, in °C"""
         return self._requestValueHP(
             "2/Sensor/TankTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def tank_setpoint_temperature(self) -> float:
-        """ Returns the hot water tank setpoint (target) temperature, in °C """
+        """Returns the hot water tank setpoint (target) temperature, in °C"""
         return self._requestValueHP(
             "2/Operation/TargetTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def is_tank_heating_enabled(self) -> bool:
-        """ Returns if the tank heating is currently enabled"""
-        return self._requestValueHP("2/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con") == "on"
+        """Returns if the tank heating is currently enabled"""
+        return (
+            self._requestValueHP("2/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con")
+            == "on"
+        )
 
     @property
     def is_tank_powerful(self) -> bool:
-        """ Returns if the tank is in powerful state """
-        return self._requestValueHP("2/Operation/Powerful/la", "m2m:rsp/pc/m2m:cin/con") == 1
+        """Returns if the tank is in powerful state"""
+        return (
+            self._requestValueHP("2/Operation/Powerful/la", "m2m:rsp/pc/m2m:cin/con")
+            == 1
+        )
 
     def set_tank_heating_enabled(self, powerful_active: bool):
-        """ Whether to turn the water tank heating on(True) or off(False).
+        """Whether to turn the water tank heating on(True) or off(False).
         You can confirm that it works by calling self.is_tank_heating_enabled
         """
         mode_dict = {
@@ -159,143 +176,153 @@ class DaikinAltherma:
         }
 
         payload = {
-            'con': mode_dict[powerful_active],
-            'cnf': 'text/plain:0',
+            "con": mode_dict[powerful_active],
+            "cnf": "text/plain:0",
         }
 
         self._requestValueHP("2/Operation/Powerful", "/", payload)
 
-
     # HEATING STUFF
     @property
     def indoor_temperature(self) -> float:
-        """ Returns the indoor temperature, in °C """
+        """Returns the indoor temperature, in °C"""
         return self._requestValueHP(
             "1/Sensor/IndoorTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def outdoor_temperature(self) -> float:
-        """ Returns the outdoor temperature, in °C """
+        """Returns the outdoor temperature, in °C"""
         return self._requestValueHP(
             "1/Sensor/OutdoorTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def indoor_setpoint_temperature(self) -> float:
-        """ Returns the indoor setpoint (target) temperature, in °C """
+        """Returns the indoor setpoint (target) temperature, in °C"""
         return self._requestValueHP(
             "1/Operation/TargetTemperature/la", "/m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def leaving_water_temperature(self) -> float:
-        """ Returns the heating leaving water temperature, in °C """
+        """Returns the heating leaving water temperature, in °C"""
         return self._requestValueHP(
             "1/Sensor/LeavingWaterTemperatureCurrent/la", "m2m:rsp/pc/m2m:cin/con"
         )
 
     @property
     def is_heating_enabled(self) -> bool:
-        """ Returns if the unit heating is enabled"""
-        return self._requestValueHP("1/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con") == "on"
+        """Returns if the unit heating is enabled"""
+        return (
+            self._requestValueHP("1/Operation/Power/la", "m2m:rsp/pc/m2m:cin/con")
+            == "on"
+        )
 
     @property
     def heating_mode(self) -> str:
-        """ This function name makes no sense, because it
+        """This function name makes no sense, because it
         returns whether the heat pump is heating or cooling.
         """
-        return self._requestValueHP("1/Operation/OperationMode/la", "m2m:rsp/pc/m2m:cin/con")
+        return self._requestValueHP(
+            "1/Operation/OperationMode/la", "m2m:rsp/pc/m2m:cin/con"
+        )
 
     @property
     def power_consumption(self) -> dict:
-        """ Returns the energy consumption in kWh per [D]ay, [W]eek, [M]onth """
+        """Returns the energy consumption in kWh per [D]ay, [W]eek, [M]onth"""
         return self._requestValueHP("1/Consumption/la", "m2m:rsp/pc/m2m:cin/con")
 
     def set_setpoint_temperature(self, setpoint_temperature_c: float):
-        """ Sets the heating setpoint (target) temperature, in °C"""
+        """Sets the heating setpoint (target) temperature, in °C"""
         payload = {
-            'con': setpoint_temperature_c,
-            'cnf': 'text/plain:0',
+            "con": setpoint_temperature_c,
+            "cnf": "text/plain:0",
         }
 
         self._requestValueHP("1/Operation/TargetTemperature", "/", payload)
 
     def set_heating_enabled(self, heating_active: bool):
-        """ Whether to turn the heating on(True) or off(False).
+        """Whether to turn the heating on(True) or off(False).
         You can confirm that it works by calling self.is_heating_enabled
         """
         mode_dict = {
-            True: 'on',
-            False: 'standby',
+            True: "on",
+            False: "standby",
         }
 
         payload = {
-            'con': mode_dict[heating_active],
-            'cnf': 'text/plain:0',
+            "con": mode_dict[heating_active],
+            "cnf": "text/plain:0",
         }
 
         self._requestValueHP("1/Operation/Power", "/", payload)
 
-
     @property
     def schedule_list_heating(self) -> list[Schedule]:
-        """ Returns the Schedule list heating """
-        d = self._requestValueHP("1/Schedule/List/Heating/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the Schedule list heating"""
+        d = self._requestValueHP(
+            "1/Schedule/List/Heating/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
         j = json.loads(d)
+
         def value_parser(x) -> float:
-            return float(x)/10
+            return float(x) / 10
 
         out_schedules = []
-        for schedule in j['data']:
+        for schedule in j["data"]:
             out_schedules.append(self._unmarshall_schedule(schedule, value_parser))
         return out_schedules
 
     @property
     def schedule_tank_heating(self) -> list[Schedule]:
-        """ Returns the Schedule list heating """
-        d = self._requestValueHP("2/Schedule/List/Heating/la", "/m2m:rsp/pc/m2m:cin/con")
+        """Returns the Schedule list heating"""
+        d = self._requestValueHP(
+            "2/Schedule/List/Heating/la", "/m2m:rsp/pc/m2m:cin/con"
+        )
         j = json.loads(d)
+
         def value_parser(x):
             return {
-                '2': TankScheduleEnum.OFF,
-                '1': TankScheduleEnum.COMFORT,
-                '0': TankScheduleEnum.ECO,
+                "2": TankScheduleEnum.OFF,
+                "1": TankScheduleEnum.COMFORT,
+                "0": TankScheduleEnum.ECO,
             }[x]
 
         out_schedules = []
-        for schedule in j['data']:
+        for schedule in j["data"]:
             out_schedules.append(self._unmarshall_schedule(schedule, value_parser))
         return out_schedules
 
     def set_heating_schedule(self, schedule: Schedule):
-        ''' Sets the heating schedule for the heating. '''
+        """Sets the heating schedule for the heating."""
         schedule_str = self._marshall_schedule(schedule)
-        dq = {'data': [schedule_str]}
+        dq = {"data": [schedule_str]}
 
         payload = {
-            'con': json.dumps(dq),
-            'cnf': 'text/plain:0',
+            "con": json.dumps(dq),
+            "cnf": "text/plain:0",
         }
         self._requestValueHP("1/Schedule/List/Heating", "/", payload)
 
-#    @property
-#    def is_heating_schedule_enabled(self):
-#        return self._requestValueHP("1/Schedule/Active/la",  "/m2m:rsp/pc/m2m:cin/con")
-
+    #    @property
+    #    def is_heating_schedule_enabled(self):
+    #        return self._requestValueHP("1/Schedule/Active/la",  "/m2m:rsp/pc/m2m:cin/con")
 
     @property
     def schedule_next(self) -> Schedule:
-        """ What will happen next the temperature """
+        """What will happen next the temperature"""
         d = self._requestValueHP("1/Schedule/Next/la", "/m2m:rsp/pc/m2m:cin/con")
         j = json.loads(d)
+
         def value_parser(x) -> float:
-            return float(x)/10
+            return float(x) / 10
+
         return self._unmarshall_schedule(j, value_parser)
 
-
     def print_all_status(self):
-        print(f'''
+        print(
+            f"""
 Daikin adapter: {self.adapter_ip} {self.adapter_model}
 Daikin unit: {self.unit_model} {self.unit_type}
 Daikin time: {self.unit_datetime}
@@ -309,26 +336,26 @@ Heating:
     Leaving water: {self.leaving_water_temperature}°C
     Heating mode: {self.heating_mode}
     Schedule: {self.schedule_list_heating[0]}
-    ''')
-
+    """
+        )
 
     @staticmethod
     def _unmarshall_schedule(schedule_str: str, value_parser: Callable) -> Schedule:
-        ''' Converts a schedule string to a schedule dict.
+        """Converts a schedule string to a schedule dict.
         The dict keys are the days (DaikinAltherma.DAYS), and the
         values are a dict of hour (HHMM) -> Setpoint T°
-        Ex: {'Mo': {'0000': 23.4}}'''
+        Ex: {'Mo': {'0000': 23.4}}"""
 
-        hrs = schedule_str.split('|')[2].split(';')
+        hrs = schedule_str.split("|")[2].split(";")
         i = 0
         schedule = {}
 
         for day in DaikinAltherma.DAYS:
             schedule_wk = {}
-            temps = hrs[i:i+6]
+            temps = hrs[i : i + 6]
             for c in temps:
-                ctime, ctemp = c.split(',')
-                if ctime == '':
+                ctime, ctemp = c.split(",")
+                if ctime == "":
                     continue
                 val = value_parser(ctemp)
                 schedule_wk[ctime] = val
@@ -337,26 +364,27 @@ Heating:
             schedule[day] = schedule_wk
         return schedule
 
-
     @staticmethod
     def _marshall_schedule(schedule: Schedule) -> str:
-        '''' Converts a schedule dict to a Daikin schedule string'''
+        """' Converts a schedule dict to a Daikin schedule string"""
         week_schedule = []
         for day in DaikinAltherma.DAYS:
             sday = schedule.get(day, {})
             schedule_day = []
 
-            assert(len(sday) <= 6)
+            assert len(sday) <= 6
             for hour in sorted(sday.keys()):
-                schedule_day.append(f'{hour},{int(sday[hour]*10)}')
-            padding_schedule = 6-len(schedule_day)
-            schedule_day += [','] * padding_schedule
+                schedule_day.append(f"{hour},{int(sday[hour]*10)}")
+            padding_schedule = 6 - len(schedule_day)
+            schedule_day += [","] * padding_schedule
             week_schedule += schedule_day
-        schedule_str = '$NULL|1|' + ';'.join(week_schedule)
+        schedule_str = "$NULL|1|" + ";".join(week_schedule)
         return schedule_str
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ad = DaikinAltherma("192.168.11.100")
     ad.print_all_status()
     import pprint
+
     pprint.pprint(ad._unit_api)
--- daikin_altherma/tests/test_schedule.py
+++ daikin_altherma/tests/test_schedule.py
@@ -4,21 +4,27 @@ import pprint
 from daikin_altherma import DaikinAltherma
 
 
-S1 = '$NULL|1|0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,'
+S1 = "$NULL|1|0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,;0000,180;0450,200;2300,180;,;,;,"
 
 
 class TestSchedule(unittest.TestCase):
-
     def test_schedule(self):
-        ''' Tests conversion from str to dict to str'''
+        """Tests conversion from str to dict to str"""
         s_dict = DaikinAltherma._unmarshall_schedule(S1)
         s_str = DaikinAltherma._marshall_schedule(s_dict)
         assert S1 == s_str
 
     def test_schedule_dict(self):
         schedule = {
-            'Mo': {'0000': 22.0},
-            'Tu': {}, 'We': {}, 'Th': {}, 'Fr': {}, 'Sa': {}, 'Su': {},
+            "Mo": {"0000": 22.0},
+            "Tu": {},
+            "We": {},
+            "Th": {},
+            "Fr": {},
+            "Sa": {},
+            "Su": {},
         }
-        back_schedule = DaikinAltherma._unmarshall_schedule(DaikinAltherma._marshall_schedule(schedule))
+        back_schedule = DaikinAltherma._unmarshall_schedule(
+            DaikinAltherma._marshall_schedule(schedule)
+        )
         assert schedule == back_schedule
